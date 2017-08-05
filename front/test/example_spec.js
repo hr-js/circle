81c2b96c9b4f8b605a4c1ec25b5367a899138015
tomoyuki-tanaka <masuminium5@gmail.com>
2017-08-05 12:45:16 +0900

    development settings

---
+++ front/test/example_spec.js
@@ -0,0 +1,7 @@
+import { expect } from 'chai'
+
+describe('expample test', () => {
+  it('1 is 1', () => {
+    expect(1).to.deep.equal(1)
+  })
+})
---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const path = require('path')
+const webpack = require('webpack')
+
+module.exports = {
+  context: __dirname + '/front/src',
+  entry: {
+    index: './index.jsx'
+  },
+  output: {
+    path: path.join(__dirname, '/front/bundle'),
+    filename: '[name].js'
+  },
+  module: {
+    rules: [{
+      test: /\.jsx?$/,
+      use: 'babel-loader'
+    }]
+  },
+  resolve: {
+    modules: [
+      'node_modules',
+      path.resolve('/front/src')
+    ],
+    extentions: ['.js', '.jsx']
+  },
+  devServer: {
+    contentBase: 'front/bundle',
+    inline: true,
+    hot: true,
+    historyApiFallback: true,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, PATCH, OPTIONS',
+      'Access-Control-Allow-Headers': 'X-Requestd-With, content-type, Authorizations'
+    }
+  },
+  devtool: 'source-map',
+  plugins: [
+    new webpack.hotModuleReplacementPlugin()
+  ]
+}
